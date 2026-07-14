1749fcd9c249d8e8d9a4e34b2c72996bba2bf4d6
Heir-of-God <glebtatarincev08@gmail.com>
2026-07-14 12:28:43 +0300

    feat: project init, completed exercise 1.6

---
+++ part1/exercises/unicafe/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import "./App.css";
+
+function Title({ titleText }) {
+  return <h2>{titleText}</h2>;
+}
+
+function Button({ onClick, text }) {
+  return <button onClick={onClick}>{text}</button>;
+}
+
+function StatisticsElement({ text, value }) {
+  return (
+    <p>
+      {text} {value}
+    </p>
+  );
+}
+
+function App() {
+  const [goodCounter, setGoodCounter] = useState(0);
+  const [neutralCounter, setNeutralCounter] = useState(0);
+  const [badCounter, setBadCounter] = useState(0);
+
+  const appText = "Give feedback";
+  const statisticText = "Statistics";
+  const feedbackOptions = ["good", "neutral", "bad"];
+
+  function setGood(new_val) {
+    setGoodCounter(new_val);
+  }
+
+  function setNeutral(new_val) {
+    setNeutralCounter(new_val);
+  }
+
+  function setBad(new_val) {
+    setBadCounter(new_val);
+  }
+
+  return (
+    <>
+      <Title titleText={appText} />
+      <Button
+        onClick={() => setGood(goodCounter + 1)}
+        text={feedbackOptions[0]}
+      />
+      <Button
+        onClick={() => setNeutral(neutralCounter + 1)}
+        text={feedbackOptions[1]}
+      />
+      <Button
+        onClick={() => setBad(badCounter + 1)}
+        text={feedbackOptions[2]}
+      />
+      <Title titleText={statisticText} />
+      <StatisticsElement text={feedbackOptions[0]} value={goodCounter} />
+      <StatisticsElement text={feedbackOptions[1]} value={neutralCounter} />
+      <StatisticsElement text={feedbackOptions[2]} value={badCounter} />
+    </>
+  );
+}
+
+export default App;
