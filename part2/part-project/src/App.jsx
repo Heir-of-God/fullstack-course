589b10377f9ccdfc423ba6acecbc5428f0249e25
Heir-of-God <glebtatarincev08@gmail.com>
2026-07-19 09:52:25 +0300

    feat: completed part 2.c

--- part2/part-project/src/App.jsx
+++ part2/part-project/src/App.jsx
@@ -1,10 +1,17 @@
 import Note from "./components/Note";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App({ passedNotes }) {
-  const [notes, setNotes] = useState(passedNotes);
+function App() {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/notes").then((response) => {
+      setNotes(response.data);
+    });
+  }, []);
 
   const notesToShow = showAll
     ? notes
--- part2/part-project/src/main.jsx
+++ part2/part-project/src/main.jsx
@@ -1,24 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<App passedNotes={notes} />);
+root.render(<App />);
